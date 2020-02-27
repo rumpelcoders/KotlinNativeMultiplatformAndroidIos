8ab30582433f0e9f895dcccfa004ee12c32ac908
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2020-02-27 15:02:46 +0100

    basic oauth functionality

--- Core/src/commonMain/kotlin/Common.kt
+++ Core/src/commonMain/kotlin/Common.kt
@@ -1,30 +1,91 @@
 package com.jarhoax.multiplatform.core
 
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import kotlinx.coroutines.*
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.url
+import io.ktor.http.ContentType
+import io.ktor.http.Url
+import io.ktor.http.contentType
+import io.ktor.http.withCharset
+import io.ktor.utils.io.charsets.Charsets
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 expect fun platformName(): String
 
-fun createApplicationScreenMessage() : String {
+
+fun createApplicationScreenMessage(): String {
     return "Kotlin Rocks on ${platformName()}"
 }
 
 internal expect val ApplicationDispatcher: CoroutineDispatcher
 
 
-class SlackApi {
-    private val client = HttpClient()
+class SlackApi(val clientId: String, val clientSecret: String) {
 
-    private val address = Url(slackApiBaseUrl+"api.test")
+    private val client = HttpClient {
 
-    fun about(callback: (String) -> Unit) {
+    }
+
+    private val scope = "users.profile:write"
+
+    private val callback = "http://www.test.com"
+
+    fun authorize(callback: (String) -> Unit) {
+        val address =
+            Url("https://slack.com/oauth/authorize?client_id=$clientId&scope=$scope&redirect_uri=$callback")
         GlobalScope.apply {
             launch(ApplicationDispatcher) {
                 val result: String = client.get {
-                    url(this@SlackApi.address.toString())
+                    url(address.toString())
                 }
                 callback(result)
+            }
+        }
+    }
+
+    private var token: String? = null
+
+    fun setState(
+        state: String,
+        callback: (String) -> Unit,
+        emoji: String
+    ) {
+        val address = Url(slackApiBaseUrl + "/users.profile.set")
+
+        GlobalScope.apply {
+            launch(ApplicationDispatcher) {
+                val result: String = client.post {
+                    url(address.toString())
+                    header("Authorization", "Bearer $token")
+                    contentType(ContentType.Application.Json.withCharset(Charsets.UTF_8))
+                    body = """
+{
+    "profile": {
+        "status_text": "$state",
+        "status_emoji": "$emoji",
+        "status_expiration": 0
+    }
+}"""
+                }
+                callback(result)
+            }
+        }
+    }
+
+    fun onRedirectCodeReceived(url: String) {
+        val code = Url(url).parameters["code"]
+        val address =
+            Url("${slackApiBaseUrl}oauth.access?client_id=$clientId&scope=$scope&redirect_uri=$callback&client_secret=$clientSecret&code=$code")
+        GlobalScope.apply {
+            launch(ApplicationDispatcher) {
+                val result: String = client.get {
+                    url(address.toString())
+                }
+                token = result
             }
         }
     }
--- app/src/main/java/com/jarhoax/multiplatform/demo/MainActivity.kt
+++ app/src/main/java/com/jarhoax/multiplatform/demo/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.jarhoax.multiplatform.demo
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.jarhoax.multiplatform.core.SlackApi
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SlackApi().about {
+        val slackApi = SlackApi("TODO","TODO")
+
+        slackApi.setState("Am i a train?", {
             GlobalScope.apply {
                 launch(Dispatchers.Main) {
-                    main_text.text = it
+                    Log.d("MainActivity",it)
+                }
+            }
+        }, ":mountain_railway:")
+        slackApi.authorize {
+            GlobalScope.apply {
+                launch(Dispatchers.Main) {
+                    Log.d("MainActivity",it)
+                    web_view.getSettings().setJavaScriptEnabled(true);
+                    web_view.setWebViewClient(object : WebViewClient() {
+                        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                            if (url.startsWith("http://www.test.com")) {
+                                Log.d("MainActivity",url)
+                                slackApi.onRedirectCodeReceived(url)
+                            }
+                            return false
+                        }
+                    })
+                    web_view.loadDataWithBaseURL("", it, "text/html", "UTF-8", "")
                 }
             }
         }
