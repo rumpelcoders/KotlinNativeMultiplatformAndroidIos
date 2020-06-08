120db22c7ebd9032f013156f250ca2adc1958ed5
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2020-06-08 07:38:54 +0200

    minor cleanup

--- app/src/main/java/com/rumpel/mpp/statesonsteroids/android/ui/geofencing/WifiMonitoringService.kt
+++ app/src/main/java/com/rumpel/mpp/statesonsteroids/android/ui/geofencing/WifiMonitoringService.kt
@@ -9,6 +9,7 @@ import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.net.wifi.WifiManager
 import android.os.IBinder
+import com.rumpel.mpp.statesonsteroids.android.R
 import com.rumpel.mpp.statesonsteroids.android.util.assetJsonString
 import com.rumpel.mpp.statesonsteroids.core.SlackApi
 import com.rumpel.mpp.statesonsteroids.core.loadAutomationEntries
@@ -28,7 +29,7 @@ class WifiMonitoringService : Service() {
                 filterEntries(entries, "disconnected")
 
 
-            setSlackState(slackApi, actionItems)
+            slackApi.setSlackState(actionItems)
 
         }
 
@@ -40,52 +41,23 @@ class WifiMonitoringService : Service() {
 
         override fun onAvailable(network: Network?) {
             val slackApi = SlackApi(assetJsonString(this@WifiMonitoringService))
-            val wifiManager =
-                applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            deviceName = wifiManager.deviceName()
+            getDeviceName()
 
 
             val entries = loadAutomationEntries()
             val actionItems = filterEntries(entries, "connected")
 
-            setSlackState(slackApi, actionItems)
+            slackApi.setSlackState(actionItems)
         }
 
-        private fun filterEntries(
-            entries: List<AutomationEntry>,
-            automationAction: String
-        ): List<AutomationEntry> =
-            entries.filter { it.automationData is AutomationData.WifiAutomationData }
-                .filter { it.automationAction == automationAction }
-                .filter {
-                    (it.automationData as AutomationData.WifiAutomationData).ssid == deviceName?.replace(
-                        "\"",
-                        ""
-                    )
-                }
+
     }
 
-    private fun setSlackState(
-        slackApi: SlackApi,
-        actionItems: List<AutomationEntry>
-    ) {
-        slackApi.authorize {
-            if (it.isAuthenticated) {
-                //we are in a very unstable network situation here as wifi just did something.
-                //let's wait a bit for it to settle down.
-                runBlocking {
-                    delay(5000)
-                }
-                actionItems.forEach { item ->
-                    slackApi.setState(
-                        item.statusText,
-                        item.statusEmoji,
-                        item.statusExpiration.toInt()
-                    ) { }
-                }
-            }
-        }
+    private fun getDeviceName() {
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        deviceName = wifiManager.deviceName()
     }
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val connectivityManager =
@@ -95,7 +67,11 @@ class WifiMonitoringService : Service() {
             .build()
         connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
         val createNotification =
-            createNotification(this, "Steroids is watching you", "For reasons.")
+            createNotification(
+                this,
+                getString(R.string.service_title),
+                getString(R.string.service_description)
+            )
         startForeground(1, createNotification.build())
         return START_NOT_STICKY
     }
@@ -115,3 +91,35 @@ fun WifiManager.deviceName(): String = connectionInfo.ssid.run {
     if (this.contains("<unknown ssid>")) "UNKNOWN" else this
 }
 
+fun SlackApi.setSlackState(actionItems: List<AutomationEntry>) {
+    authorize {
+        if (it.isAuthenticated) {
+            //we are in a very unstable network situation here as wifi just did something.
+            //let's wait a bit for it to settle down.
+            runBlocking {
+                delay(5000)
+            }
+            actionItems.forEach { item ->
+                setState(
+                    item.statusText,
+                    item.statusEmoji,
+                    item.statusExpiration.toInt()
+                ) { }
+            }
+        }
+    }
+}
+
+private fun filterEntries(
+    entries: List<AutomationEntry>,
+    automationAction: String,
+    deviceName: String?
+): List<AutomationEntry> =
+    entries.filter { it.automationData is AutomationData.WifiAutomationData }
+        .filter { it.automationAction == automationAction }
+        .filter {
+            (it.automationData as AutomationData.WifiAutomationData).ssid == deviceName?.replace(
+                "\"",
+                ""
+            )
+        }
